b0eb319c576168c01349432d84f861e74bed0c13
user name <harshj6680@gmail.com>
2023-09-08 18:59:34 +0530

    read more and read less is working perfectly now next things are pagination or infinite scroll

---
+++ Models/InfiniteScrollViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMC_Donation.Models
+{
+    public class InfiniteScrollViewModel
+    {
+        public List<ItemsWithUserViewModel> ItemsWithUserDetails { get; set; }
+        public bool HasMoreItems { get; set; }
+    }
+}
